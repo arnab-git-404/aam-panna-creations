1ac1937aa45cf16274010249a8dfc5554b07ed80
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-01 17:36:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Hero from "@/components/Home/Hero";
-import Testimonials from '@/components/Home/testimonials';
 import Services from '@/components/Home/ServicesSection';
+import Testimonials from '@/components/Home/Testimonials';
+import WhoWeAre from '@/components/Home/whoWeAre';
+import Form from '@/components/Home/Form';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <WhoWeAre />
       <Services />
       <Testimonials />
-      {/* Add other sections here as needed */}
+      <Form />
     </main>
   );
 }
