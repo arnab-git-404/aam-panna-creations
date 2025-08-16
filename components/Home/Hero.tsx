ec4654bf3f34a71ad78fdf9dfd9da66d94c5d5a7
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-16 17:16:28 +0530

    Update Hero.tsx

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="container h-screen max-w-full bg-[#D9D9D9] flex items-center justify-center">
+      <div className="container h-screen max-w-full bg-[url('/hero-bg.png')] bg-cover flex items-center justify-center">
 
         <Image
           src="/hero-section-logo.png"
