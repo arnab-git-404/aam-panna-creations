3fd7ed1be1570e54be50e816be380e2435ad5f63
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-26 18:17:20 +0530

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,14 @@
 
 "use client";
-const Team = () => {
 
+const Test = () => {
   return (
     <div className="min-h-screen  ">
       <h1 className="text-4xl font-bold text-center mt-12">Testing Page</h1>
     </div>
   );
 };
+export default Test;
 
 // // // // // =====================================================================================================================================================
 
