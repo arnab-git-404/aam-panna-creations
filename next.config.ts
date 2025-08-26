85931bbe21227c3c900374ffeb303a8185df8363
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-08-26 18:07:44 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['gsap', 'locomotive-scroll'],
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   transpilePackages: ['gsap', 'locomotive-scroll'],
+//   experimental: {
+//     esmExternals: false
+//   }
+// }
+
+// module.exports = nextConfig
