0d4991f8ae6b859bc9ba67222f1d8e075a5913d7
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-01 23:29:37 +0530

    Update page.tsx

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -156,27 +156,27 @@ const PrivacyPolicy = () => {
                       Usage Data
                     </h4>
                     <p className="text-gray-600 leading-relaxed mb-3">
-                      We may also collect information on how the Service is
+                      {`We may also collect information on how the Service is
                       accessed and used ("Usage Data"). This Usage Data may
                       include information such as Your computer's Internet
                       Protocol address (e.g., IP address), browser type, browser
                       version, the pages of our Service that You visit, the time
                       and date of Your visit, the time spent on those pages,
-                      unique device identifiers, and other diagnostic data.
+                      unique device identifiers, and other diagnostic data.`}
                     </p>
                     <p className="text-gray-600 leading-relaxed mb-3">
-                      When You access the Service by or through a mobile device,
+                      {`When You access the Service by or through a mobile device,
                       We may collect certain information automatically,
                       including, but not limited to, the type of mobile device
                       You use, Your mobile device unique ID, the IP address of
                       Your mobile device, Your mobile operating system, the type
                       of mobile Internet browser You use, unique device
-                      identifiers, and other diagnostic data.
+                      identifiers, and other diagnostic data.`}
                     </p>
                     <p className="text-gray-600 leading-relaxed">
-                      We may also collect information that Your browser sends
+                      {`We may also collect information that Your browser sends
                       whenever You visit our Service or when You access the
-                      Service by or through a mobile device.
+                      Service by or through a mobile device.`}
                     </p>
                   </div>
                 </div>
