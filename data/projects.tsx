fac2969526ce5a0910afcb4c05a6f96115a85e95
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-03 01:24:21 +0530

    Update projects.tsx

--- data/projects.tsx
+++ data/projects.tsx
@@ -15,11 +15,55 @@ export const projects: Project[] = [
         title: "Web Design & Development",
         icon: "/services/Code.svg",
         description: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit id venenatis pretium risus euismod dictum egestas orci netus feugiat ut egestas ut sagittis tincidunt phasellus elit etiam cursus orci in. Id sed montes.",
+            "Your website is your digital storefront — and we make it unforgettable. At Aam Panna Creations, we craft visually stunning, fast, and mobile-friendly websites. Every design is custom-built to tell your story and engage your audience. With us, your online presence doesn’t just exist — it inspires, connects, and grows."
         ],
         projectName: "ClayLab",
-        projectDes: "A platform for Child Education.",
+        projectDes: "We built ClayLab Foundation’s website from scratch and strengthened their digital presence with impactful design and social media strategy. By aligning their platform with their mission, we helped position them as a credible and inspiring education-focused brand.",
         liveLink: "https://claylab-ver.vercel.app/",
-    }
+    },
+    {
+        id: 2,
+        title: "Social Media Management",
+        icon: "/services/Code.svg",
+        description: [
+            "Aam Panna Creations helps businesses shine online with impactful social media management. We create tailored strategies that connect with your audience and strengthen your brand identity. From content planning to daily engagement, we handle it all to grow your digital presence. Our goal is to turn followers into loyal customers and conversations into lasting relationships."
+        ],
+        projectName: "Project Kancha",
+        projectDes: "We partnered with Project Kancha to elevate their digital presence and storytelling reach. From designing their website to managing social media content, we helped amplify their innovative programs—like Daring Classrooms, Street-to-Stage workshops, and theatre for social change across underserved communities.",
+        liveLink: "https://www.instagram.com/projectkancha?igsh=c2wxNTVnZGYzd3M0",
+    },
+    {
+        id: 3,
+        title: "Personal Branding",
+        icon: "/services/Code.svg",
+        description: [
+        "Your story is unique — and we make sure the world sees it that way. At Aam Panna Creations, we shape personal brands that inspire trust and recognition. Through design, content, and strategy, we amplify your voice across digital platforms. With us, you don’t just have a presence — you create influence and leave a mark."
+        ],
+        projectName: "Prathi Tandon",
+        projectDes: "At Aam Pannaa Creations, we partnered with Prathi Tandon to amplify her personal brand through podcast creation and social media building. From concept to execution, our team ensured her voice reached the right audience. The results spoke for themselves — one of her videos went viral, crossing 110K impressions within just 8 hours, showcasing the power of authentic storytelling and strategic branding.",
+        liveLink: "https://www.instagram.com/prathi_tandon?igsh=cTN2ZGpzbDl0aDhn",
+    },
+    {
+        id: 4,
+        title: "Podcast editing",
+        icon: "/services/Code.svg",
+        description: [
+            "Great conversations deserve great sound — and that’s where we come in. At Aam Panna Creations, we transform raw recordings into podcasts that shine. We edit, enhance, and add the right touch of creativity so your voice stands out. With us, your podcast isn’t just heard — it’s remembered."
+        ],
+        projectName: "Prathi Tandon",
+        projectDes: "We partnered with Prathi Tandon in launching her podcast and building a strong social media presence around it. From concept creation to execution, our team at Aam Pannaa Creations worked on end-to-end podcast production and strategic content building. One of the campaign videos went viral, achieving 110K+ impressions in just 8 hours, reflecting the impact of our collaboration.",
+        liveLink: "https://www.youtube.com/@escape.velocity_drops",
+    },
+    {
+        id: 5,
+        title: "Mentorship and Consulting",
+        icon: "/services/Code.svg",
+        description: [
+            "Every great journey needs the right guidance — and we’re here to be that for you. At Aam Panna Creations, our mentorship and consulting unlock your potential and fuel growth. We blend experience with creativity to give you clear direction and actionable advice. With us, you don’t just get answers — you gain a partner in your success story."
+        ],
+        projectName: "",
+        projectDes: "",
+        liveLink: "",
+    },
 
 ]
