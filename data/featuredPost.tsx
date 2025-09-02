45893aa2c35730f88b9c79022e73d167248623ae
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-02 13:04:57 +0530

    Create featuredPost.tsx

---
+++ data/featuredPost.tsx
@@ -0,0 +1,50 @@
+interface Post {
+  id: number;
+  title: string;
+  link: string;
+  img: string;
+  description: string;
+}
+
+export const featuredPosts: Post[] = [
+  {
+    id: 1,
+    title: "PITCH COFEE PODCAST",
+    link: "https://www.youtube.com/watch?v=qm32j52cT4o",
+    img: "/featuredPost/PITCH COFEE PODCAST.svg",
+    description:
+      "Outstanding service and incredible attention to detail. The team exceeded our expectations and delivered results that transformed our business.",
+  },
+  {
+    id: 2,
+    title: "WOMEN STARTUP STORY",
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7204053365025071104?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_media_list%3BWCUjJ3uySm2DyGI3x7ZAmw%3D%3D",
+    img: "/featuredPost/WOMEN STARTUP STORY.svg",
+    description:
+      "The creative vision and professional execution made this collaboration exceptional. Highly recommend their innovative approach to complex challenges.",
+  },
+  {
+    id: 3,
+    title: "THE WRITING EXPERIMENT",
+    link: "https://divyangna.substack.com/p/women-who-inspire-micro-interview",
+    img: "/featuredPost/THE WRITING EXPERIMENT.svg",
+    description:
+      "Working with this team was a game-changer for our brand. Their strategic insights and flawless execution delivered remarkable results.",
+  },
+  {
+    id: 4,
+    title: "DREAMER’S TALK",
+    link: "https://www.instagram.com/reel/DFDIW8JqJpW/",
+    img: "/featuredPost/DREAMER’S TALK.svg",
+    description:
+      "Exceptional quality and professionalism throughout the entire process. The results speak for themselves - truly outstanding work.",
+  },
+  {
+    id: 5,
+    title: "India news",
+    link: "https://theincpost.com/india-news/nikita-singh-aam-pannaa-creations-success-story/",
+    img: "/featuredPost/India news.svg",
+    description:
+      "Professional, reliable, and incredibly talented. They brought our vision to life better than we could have imagined.",
+  }
+];
