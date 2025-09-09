1530ded8f9d1a03831e251ef2b8704249030dded
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-09 19:19:55 +0530

    Update testimonials.tsx

--- data/testimonials.tsx
+++ data/testimonials.tsx
@@ -8,59 +8,57 @@ interface Testimonials {
 export const clientTestimonials: Testimonials[] = [
   {
     id: 1,
-    title: "Sharmila Purank",
+    title: "— Sharmila Puranik",
     role: "Founder, Luminary Pathways",
     description:
       "Nikita is a hard-working professional. She and her talented team goes above and beyond to work with strategy and system. I have no doubts on the way they will grow themselves and their brands.",
   },
   {
     id: 2,
-    title: "Prachi Tandon",
+    title: "— Prathi Tandon",
     role: "Founder, Escape Velocity",
     description:
-      "It took me seconds to close the partnership with Nikita even before I saw her samples. I have full faith on her expertise and quality of work. She will do wonders in building Aam Pannaa Creations.",
+      "It took me seconds to close the partnership with Nikhita even before I saw her samples. I have full faith on her expertise and quality of work. She will do wonders in building Aam Pannaa Creations.",
   },
   {
     id: 3,
     title: "Rashi",
-    role: "Founder, Legum advisors",
+    role: "Founder, Lagum advisors",
     description:
       "I truly appreciate the entire team for their patience and for attentively working on my needs and vision. Wishing Aam Pannaa Creations the very best for all their future endeavors!",
   },
   {
-    id: 7,
-    title: "Isha",
-    role: "Founder, Noor by Hansa",
-    description: 
-    "so, so lovely to speak with you - i'm so glad i reached out! it really means a lot!",
-  },
-  {
-    id: 8,
-    title: "Ritika",
-    role: "Founder, The Green Story",
-    description: 
-    "Hey, first of all, dil se thank you! The session was very well delivered and received! Everyone was so engaged. Thank you for doing this - I owe you one!",
+    id: 4,
+    title: "- Vivek",
+    role: "Claylab Foundation",
+    description:
+      "It has been a great association with Nikita and Godhuli. Over time, our trust has only grown stronger, and with their expertise, they are amplifying our presence meaningfully.",
   },
   {
     id: 5,
-    title: "Pramod Bhurji",
-    role: "Entrepreneur, Manager, PIEDS BITS Pilani",
+    title: "Aanandika",
+    role: "",
     description:
-    "Thank you, Nikita. It was a wonderful session. We look forward to many more interactions with our startups.",
+      "Working with Aam Panna and Nikita has been a delight. She has come to my rescue as a guide very often and has been a constant cheerleader. We also collaborated on some assignments and it was a smooth experience as well, ensuring the best results are delivered to the client. Working with her was a cakewalk only because she knows her work well, could give clear and precise instructions and most importantly, pays on time!",
   },
   {
-    id: 4,
-    title: "Gayatri Singh",
-    role: "",
-    description: 
-    "Shoutout to Nikita Singh for taking out time and speaking to me over a call and being so generous with giving me her insights from the experiences she has had as a marketer. Founder to founder calls are always interesting and one gets to learn a lot - thank you so much, Nikita! Guys, if you are a startup looking for advice on how to go about marketing your product/company, please consider getting on a discovery call with Nikita! You won't be disappointed.",
-  },
-    {
     id: 6,
-    title: "Dhanashree",
+    title: "Jyoti",
     role: "",
     description:
-    "I was feeling really tense and stressed when my last mentor couldn't start the sessions, and things weren't going as planned. But you have truly been like sunshine during this time, restoring my faith that I can do it with your guidance. I can't thank you enough for all the effort and support you've given me. Words may never be enough, but I'm genuinely grateful for you and everything you do. I understand that you're really busy, but I would love to have a long-term association with you as my mentor whenever you have the time. I'm always willing to learn and grow under your guidance. Also, Di, I do freelancing, mostly in content writing. If you ever come across any opportunities in the future, please do consider me.",
-    },
-  
-]
+      "I was super happy to receive the output as i exactly envisioned. The team along with Nikita, have been really supportive throughout the process, to bring my vision to life. ",
+  },
+  {
+    id: 7,
+    title: "- Vidushi",
+    role: "",
+    description:
+      "Nikita has been extremely creative and professional in bringing life to my stories and vision. She truly knows how to translate ideas into impactful work.",
+  },
+  {
+    id: 8,
+    title: "- Little Pineapple - Celsi",
+    role: "",
+    description: "Team APC put their heart and expertise into making every process smooth. With their support, I was able to build confidence in marketing — something that had always felt daunting since I started my venture. I can truly vouch for this talented team that is dedicated to building brands successfully."
+  },
+];
