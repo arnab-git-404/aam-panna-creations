ed00cf6406c94e02395118685fbee9ca1ab8ec87
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-09 19:19:52 +0530

    Update projects.tsx

--- data/projects.tsx
+++ data/projects.tsx
@@ -13,9 +13,9 @@ export const projects: Project[] = [
     {
         id: 1,
         title: "Web Design & Development",
-        icon: "/services/Code.svg",
+        icon: "/projects/Web Design & Development.jpg",
         description: [
-            "Your website is your digital storefront — and we make it unforgettable. At Aam Panna Creations, we craft visually stunning, fast, and mobile-friendly websites. Every design is custom-built to tell your story and engage your audience. With us, your online presence doesn’t just exist — it inspires, connects, and grows."
+            "At Aam Pannaa Creations we don’t just build websites, we create digital experiences. From research and UI/UX design to content, development, and deployment, every step is fueled by creativity. The result is a site that’s visually appealing, fully functional, mobile-friendly, and enriched with smooth animations. Your website becomes more than a presence, it becomes an experience that inspires and engages.",
         ],
         projectName: "ClayLab",
         projectDes: "We built ClayLab Foundation’s website from scratch and strengthened their digital presence with impactful design and social media strategy. By aligning their platform with their mission, we helped position them as a credible and inspiring education-focused brand.",
@@ -24,9 +24,9 @@ export const projects: Project[] = [
     {
         id: 2,
         title: "Social Media Management",
-        icon: "/services/Code.svg",
+        icon: "/projects/Social Media Management.svg",
         description: [
-            "Aam Panna Creations helps businesses shine online with impactful social media management. We create tailored strategies that connect with your audience and strengthen your brand identity. From content planning to daily engagement, we handle it all to grow your digital presence. Our goal is to turn followers into loyal customers and conversations into lasting relationships."
+"At Aam Pannaa Creations, we don’t just post content ,we craft a social presence. With the right mix of strategy, consistency, and creativity, we design content plans that amplify your voice, engage your audience, and build trust. Backed by analytics and daily support, we ensure your brand shows up with purpose, frequency, and impact  turning engagement into growth and followers into loyal advocates.",
         ],
         projectName: "Project Kancha",
         projectDes: "We partnered with Project Kancha to elevate their digital presence and storytelling reach. From designing their website to managing social media content, we helped amplify their innovative programs—like Daring Classrooms, Street-to-Stage workshops, and theatre for social change across underserved communities.",
@@ -35,9 +35,9 @@ export const projects: Project[] = [
     {
         id: 3,
         title: "Personal Branding",
-        icon: "/services/Code.svg",
+        icon: "/projects/Personal Branding.svg",
         description: [
-        "Your story is unique — and we make sure the world sees it that way. At Aam Panna Creations, we shape personal brands that inspire trust and recognition. Through design, content, and strategy, we amplify your voice across digital platforms. With us, you don’t just have a presence — you create influence and leave a mark."
+"At Aam Panna Creations, we turn your authentic story into influence. Through sharp strategy, design, and content, we build your credibility and position you as an industry expert. Beyond presence, we help you own your voice, grow your impact, and leave a lasting mark.",
         ],
         projectName: "Prathi Tandon",
         projectDes: "At Aam Pannaa Creations, we partnered with Prathi Tandon to amplify her personal brand through podcast creation and social media building. From concept to execution, our team ensured her voice reached the right audience. The results spoke for themselves — one of her videos went viral, crossing 110K impressions within just 8 hours, showcasing the power of authentic storytelling and strategic branding.",
@@ -46,23 +46,23 @@ export const projects: Project[] = [
     {
         id: 4,
         title: "Podcast editing",
-        icon: "/services/Code.svg",
+        icon: "/projects/Podcast editing.svg",
         description: [
-            "Great conversations deserve great sound — and that’s where we come in. At Aam Panna Creations, we transform raw recordings into podcasts that shine. We edit, enhance, and add the right touch of creativity so your voice stands out. With us, your podcast isn’t just heard — it’s remembered."
+"At Aam Panna Creations, we turn conversations into shareable stories. From editing and creative direction to video cuts and final distribution, we shape podcasts that don’t just sound good , they stick with your audience.",
         ],
-        projectName: "Prathi Tandon",
+        projectName: "Prathi Tandon ",
         projectDes: "We partnered with Prathi Tandon in launching her podcast and building a strong social media presence around it. From concept creation to execution, our team at Aam Pannaa Creations worked on end-to-end podcast production and strategic content building. One of the campaign videos went viral, achieving 110K+ impressions in just 8 hours, reflecting the impact of our collaboration.",
         liveLink: "https://www.youtube.com/@escape.velocity_drops",
     },
     {
         id: 5,
         title: "Mentorship and Consulting",
-        icon: "/services/Code.svg",
+        icon: "/projects/Mentorship and Consulting.svg",
         description: [
-            "Every great journey needs the right guidance — and we’re here to be that for you. At Aam Panna Creations, our mentorship and consulting unlock your potential and fuel growth. We blend experience with creativity to give you clear direction and actionable advice. With us, you don’t just get answers — you gain a partner in your success story."
+        "At Aam Panna Creations, we guide founders and brands with clarity and strategy. Through one-on-one consultations , whether one-time or monthly , we offer the right mix of direction, support, and action steps. Our goal: help you build a brand that grows with strategy, creativity, and confidence.",
         ],
-        projectName: "",
-        projectDes: "",
+        projectName: "Isha",
+        projectDes: "Our founder, Nikita, had the pleasure of mentoring Isha, a young solo founder in the D2C space. She reached out overwhelmed, juggling burnout, mental health struggles, and the pressure of running everything alone.\n\n In just two one-hour sessions, she gained clarity, confidence, and a roadmap to lead herself and her business. Key takeaways: growth isn’t just ads and numbers, small wins matter, failures are inevitable, and your mental standards are crucial.\n\n Mentorship, for us, is about offering perspective, empathy, and actionable guidance helping founders realize they’re not alone.",
         liveLink: "",
     },
 
