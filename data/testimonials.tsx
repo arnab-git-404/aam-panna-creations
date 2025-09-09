a8e9c1bfa024ac6f76bc33677c0dce973a4fa737
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-10 00:33:15 +0530

    Update testimonials.tsx

--- data/testimonials.tsx
+++ data/testimonials.tsx
@@ -8,24 +8,24 @@ interface Testimonials {
 export const clientTestimonials: Testimonials[] = [
   {
     id: 1,
-    title: "— Sharmila Puranik",
-    role: "Founder, Luminary Pathways",
+    title: "Celsi",
+    role: "Little Pineapple",
     description:
-      "Nikita is a hard-working professional. She and her talented team goes above and beyond to work with strategy and system. I have no doubts on the way they will grow themselves and their brands.",
+      "Team APC put their heart and expertise into making every process smooth. With their support, I was able to build confidence in marketing — something that had always felt daunting since I started my venture. I can truly vouch for this talented team that is dedicated to building brands successfully.",
   },
   {
     id: 2,
-    title: "— Prathi Tandon",
-    role: "Founder, Escape Velocity",
+    title: "Sharmila Puranik",
+    role: "Founder, Luminary Pathways",
     description:
-      "It took me seconds to close the partnership with Nikhita even before I saw her samples. I have full faith on her expertise and quality of work. She will do wonders in building Aam Pannaa Creations.",
+      "Nikhita is a hard working professional. She and her talented team goes above and beyond to work with strategy and system. I have no doubts on the way they will grow themselves and their brands.",
   },
   {
     id: 3,
-    title: "Rashi",
-    role: "Founder, Lagum advisors",
+    title: "Prathi Tandon",
+    role: "Founder, Escape Velocity",
     description:
-      "I truly appreciate the entire team for their patience and for attentively working on my needs and vision. Wishing Aam Pannaa Creations the very best for all their future endeavors!",
+      "It took me seconds to close the partnership with Nikhita even before I saw her samples. I have full faith on her expertise and quality of work. She will do wonders in building Aam Pannaa Creations.",
   },
   {
     id: 4,
@@ -36,29 +36,53 @@ export const clientTestimonials: Testimonials[] = [
   },
   {
     id: 5,
+    title: "Jyoti",
+    role: "Enamel Artist",
+    description:
+      "I was super happy to receive the output as i exactly envisioned. The team along with Nikita, have been really supportive throughout the process, to bring my vision to life. ",
+  },
+  {
+    id: 6,
     title: "Aanandika",
     role: "",
     description:
       "Working with Aam Panna and Nikita has been a delight. She has come to my rescue as a guide very often and has been a constant cheerleader. We also collaborated on some assignments and it was a smooth experience as well, ensuring the best results are delivered to the client. Working with her was a cakewalk only because she knows her work well, could give clear and precise instructions and most importantly, pays on time!",
   },
   {
-    id: 6,
-    title: "Jyoti",
-    role: "",
-    description:
-      "I was super happy to receive the output as i exactly envisioned. The team along with Nikita, have been really supportive throughout the process, to bring my vision to life. ",
-  },
-  {
     id: 7,
-    title: "- Vidushi",
-    role: "",
+    title: "-Rashi Mittal",
+    role: "Founder, Lagum Advisors",
     description:
-      "Nikita has been extremely creative and professional in bringing life to my stories and vision. She truly knows how to translate ideas into impactful work.",
+      "I truly appreciate the entire team for their patience and for attentively working on my needs and vision. Wishing Aam Pannaa Creations the very best for all their future endeavors!",
   },
   {
     id: 8,
-    title: "- Little Pineapple - Celsi",
+    title: "Gayatri Singh",
     role: "",
-    description: "Team APC put their heart and expertise into making every process smooth. With their support, I was able to build confidence in marketing — something that had always felt daunting since I started my venture. I can truly vouch for this talented team that is dedicated to building brands successfully."
+    description: "Shoutout to Nikita Singh for taking out time and speaking to me over a call and being so generous with giving me her insights from the experiences she has had as a marketer. Founder to founder calls are always interesting and one gets to learn a lot - thank you so much, Nikita!"
+  },
+  {
+    id: 9,
+    title: "Isha",
+    role: "Founder, Noor by Hansa",
+    description: "so, so lovely to speak with you  i'm so glad i reached out! it really means a lot!"
+  },
+  {
+    id: 10,
+    title: "Dhanashree",
+    role: "",
+    description: "I was feeling really tense and stressed when my last mentor couldn't start the sessions, and things weren't going as planned. But you have truly been like sunshine during this time, restoring my faith that I can do it with your guidance. I can't thank you enough for all the effort and support you've given me. Words may never be enough, but I'm genuinely grateful for you and everything you do."
+  },
+  {
+    id: 11,
+    title: "Isha",
+    role: "",
+    description: "hey first of all, dil se thank you! The session was very well delivered and received! Everyone was so engaged....thank you for doing this...I owe you one...!"
+  },
+  {
+    id: 12,
+    title: "Manish",
+    role: "Sales Consultant",
+    description: "Always looking forward to your posts."
   },
 ];
