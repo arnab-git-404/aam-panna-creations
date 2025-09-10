f303a26c00e02743c80f6f21969ed731c77de358
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-10 10:00:34 +0530

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -741,8 +741,7 @@ const Team = () => {
         role: "Founder and Lead",
         image: "/team/Nikita.jpeg",
         linkedin: "https://www.linkedin.com/in/authornikitasingh",
-        description:
-          "Visionary leader with expertise in creative strategy and team management. Drives innovation and fosters collaborative excellence across all projects.",
+        description: "With 8+ years of experience in venture capital and startups, I’ve led impactful marketing initiatives while also authoring books and mentoring changemakers. Backed by academic roots from DU, St. Bede’s, and MICA, I bring a blend of strategy, creativity, and purpose to every project.",
       },
       {
         id: 2,
@@ -751,7 +750,7 @@ const Team = () => {
         image: "/team/AshaSingh.jpeg",
         linkedin: "https://www.linkedin.com/company/aam-pannaa-creations/",
         description:
-          "Strategic advisor with deep industry insights and extensive experience in business development and organizational growth.",
+"Asha Singh is an alumna of Miranda House, Delhi University, and IMT-Ghaziabad. She brings with her the experience of serving as a Director at a 30–35-year-old company, a role that has shaped her deep expertise in finance and strategy. With her strong academic pedigree and proven leadership, she contributes invaluable advisory support and financial acumen to the team.",
       },
       {
         id: 3,
@@ -761,7 +760,7 @@ const Team = () => {
         linkedin:
           "https://www.linkedin.com/in/digvijay-singh-4724a440/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         description:
-          "Seasoned professional providing strategic guidance and mentorship to drive organizational success and innovation.",
+"Digvijay Singh, an IIT-BHU graduate with a Johns Hopkins (USA) pedigree, brings global exposure, prize-winning design innovations, and entrepreneurial experience from building his own diagnostic company in the U.S. A seasoned professional, he adds strategic guidance, mentorship, and organizational depth to our advisory.",
       },
     ],
     leadership: [
