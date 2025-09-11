8c5a5df0b5c3ceed5615861354b8e04165dab350
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-12 02:07:35 +0530

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// // tailwind.config.js
+// const { fontFamily } = require('tailwindcss/defaultTheme')
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         bricolage: ['var(--font-bricolage-grotesque)', ...fontFamily.sans],
+//       },
+//     },
+//   },
+// }
