64494eb003f013ac73251c359e7113ae343ab6a7
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-09-12 02:40:42 +0530

    Update page.tsx

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -91,7 +91,7 @@ const BlogPostPage = () => {
     .slice(0, 3);
 
   return (
-    <div className="min-h-screen bg-background py-6 px-6 ">
+    <div className="min-h-screen bg-background py-6 px-6 font-bricolage ">
       {/* Navigation Bar */}
       <nav className="sticky top-0 z-50 bg-background/80 backdrop-blur-md border-b border-border">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
